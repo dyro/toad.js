243dda44553d986b87ecf5409dd912e7fc556149
Dylan Ross <dyro@me.com>
2013-03-26 19:29:42 -0600

    imporoved code readability. Improved JavaScript design
    Used designs from The Good Parts and Design Patterns to improved TOAD
    overall design.

--- toad.js
+++ toad.js
@@ -1,125 +1,134 @@
-(function(){
+(function() {
+    "use strict";
+    var each,
+        eachWithIndex,
+        map,
+        getFirst,
+        getLast,
+        removeIf,
+        keepIf;
 
-	// Functions used on muliple Data Types
-
-	// Can be used with: 
-	// Array,
-	// String
-	var _each = function(callback){
-		for (var i = 0; i < this.length; i++) {
-	  		callback(this[i]);
-	  	};
-	};
-
-	// Can be used with: 
-	// Array,
-	// String
-	var _eachWithIndex = function (callback) {
-	  	for (var i = 0; i < this.length; i++) {
-	  		callback(i, this[i]);
-	  	};
-	};
-
-	// Can be used with: 
-	// Array,
-	// String
-	var _map = function(callback) {
-	  	var results = [];
-	  	for (var i = 0; i < this.length; i++) {
-	  		results.push(callback(this[i]));
-  	  	};
-  	  	return results;
-	};
-
-	// Can be used with: 
-	// Array,
-	// String
-	var _getFirst = function() {
-		if(typeof(arguments[0]) === 'number') {
-			var results = [];
-            var len = (arguments[0] > this.length) ? this.length : arguments[0];
-			for(var i = 0; i < len; i++) {
-				results.push(this[i]);
-			}
-			return results;
-
-		} else {
-			return this[0];
-		};
-	};
+    // Functions used on muliple Data Types
 
     // Can be used with: 
     // Array,
     // String
-    var _getLast = function() {
-        if(typeof(arguments[0]) === 'number') {
-            var results = [];
-            var len = (arguments[0] > this.length) ? this.length : arguments[0];
-            for (var i = this.length - 1; i > (this.length-1) - len; i--) {
+    each = function(callback) {
+        var i;
+        for (i = 0; i < this.length; i++) {
+            callback(this[i]);
+        }
+    };
+
+    // Can be used with: 
+    // Array,
+    // String
+    eachWithIndex = function (callback) {
+        var i;
+        for (i = 0; i < this.length; i++) {
+            callback(this[i], i);
+        }
+    };
+
+    // Can be used with: 
+    // Array,
+    // String
+    map = function(callback) {
+        var results, i;
+        results = [];
+        for (i = 0; i < this.length; i++) {
+            results.push(callback(this[i]));
+        }
+        return results;
+    };
+
+    // Can be used with: 
+    // Array,
+    // String
+    getFirst = function(num) {
+        if (typeof num === 'number') {
+            var results, len, i;
+            results = [];
+            len = (num > this.length) ? this.length : num;
+            for (i = 0; i < len; i++) {
+                results.push(this[i]);
+            }
+            return results;
+        }
+        return this[0];
+    };
+
+    // Can be used with: 
+    // Array,
+    // String
+    getLast = function(num) {
+        if (typeof num === 'number') {
+            var results, len, i;
+            results = [];
+            len = (num > this.length) ? this.length : num;
+            for (i = this.length - 1; i > (this.length - 1) - len; i--) {
                 results.push(this[i]);
             }
             return results.reverse();
-        } else {
-            return this[this.length - 1];
-        };
+        }
+        return this[this.length - 1];
     };
 
-	// Can be used with: 
-	// Array,
-	// String
-	var _removeIf = function(callback) {
-		var results = [];
-		for (var i = 0; i < this.length; i++) {
-			if (!callback(this[i])) {
-				results.push(this[i]);
-			};
-		};
-		return results;
-	};
+    // Can be used with: 
+    // Array,
+    // String
+    removeIf = function(callback) {
+        var results, i;
+        results = [];
+        for (i = 0; i < this.length; i++) {
+            if (!callback(this[i])) {
+                results.push(this[i]);
+            }
+        }
+        return results;
+    };
 
-	// Can be used with: 
-	// Array,
-	// String
-	var _keepIf = function(callback) {
-		var results = [];
-	 	for (var i = 0; i < this.length; i++) {
-	 		if (callback(this[i])) {
-	 			results.push(this[i]);
-	 		};
-	 	};
-	 	return results;
-     }
+    // Can be used with: 
+    // Array,
+    // String
+    keepIf = function(callback) {
+        var results, i;
+        results = [];
+        for (i = 0; i < this.length; i++) {
+            if (callback(this[i])) {
+                results.push(this[i]);
+            }
+        }
+        return results;
+    };
 
-	// Prototypes
-	// Array
+    // Prototypes
+    // Array
 
     Array.prototype.clear = function() {
         return this.filter(function() { return true; });
     };
 
-	Array.prototype.each 		  = _each;
-	Array.prototype.eachWithIndex = _eachWithIndex;
-	Array.prototype.map 		  = _map;
-	Array.prototype.getFirst 	  = _getFirst;
-    Array.prototype.getLast       = _getLast;
-	Array.prototype.removeIf 	  = _removeIf;
-	Array.prototype.keepIf        = _keepIf;
+    Array.prototype.each          = each;
+    Array.prototype.eachWithIndex = eachWithIndex;
+    Array.prototype.map           = map;
+    Array.prototype.getFirst      = getFirst;
+    Array.prototype.getLast       = getLast;
+    Array.prototype.removeIf      = removeIf;
+    Array.prototype.keepIf        = keepIf;
 
-	// String
+    // String
 
-	String.prototype.reverse = function(){
-		return this.split("").reverse().join("");
-	};
+    String.prototype.reverse = function() {
+        return this.split("").reverse().join("");
+    };
 
-	String.prototype.eachChar          = _each;
-	String.prototype.eachCharWithIndex = _eachWithIndex;
-	String.prototype.map               = _map;
-	String.prototype.getFirst          = _getFirst;
-    String.prototype.getLast           = _getLast;
-	String.prototype.removeIf          = _removeIf;
-	String.prototype.keepIf            = _keepIf;
+    String.prototype.eachChar          = each;
+    String.prototype.eachCharWithIndex = eachWithIndex;
+    String.prototype.map               = map;
+    String.prototype.getFirst          = getFirst;
+    String.prototype.getLast           = getLast;
+    String.prototype.removeIf          = removeIf;
+    String.prototype.keepIf            = keepIf;
 
 }).call(this);
-
-console.log([1, 2, 3].getLast(2));
-
